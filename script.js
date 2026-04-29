158e6753566b9a092e13281e8b707e2e947c299e
ibilyk91 <ivan.bilyk95@myhunter.cuny.edu>
2026-04-29 14:19:51 -0400

    feat: add UI

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const display = document.createElement('div');
+display.textContent = 'Please make your selection:\n';
+document.body.appendChild(display);
+display.style.whiteSpace = 'pre-line'; // needed for \n to work
+
+const playerSelection = document.createElement('div');
+const rock = document.createElement('button');
+rock.textContent = 'rock';
+const paper = document.createElement('button');
+paper.textContent = 'paper';
+const scissors = document.createElement('button');
+scissors.textContent = 'scissors';
+
+playerSelection.append(rock, paper, scissors);
+document.body.appendChild(playerSelection);
+
+
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -15,13 +33,15 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let userChoice = prompt('Please enter your choice (Rock, Paper, Scissors)');
-  return userChoice;
+function getHumanChoice() { // for each button create click event & return textContent of the button clicked
+  return new Promise(resolve => { // creating new object from Promise(), which is like a blueprint
+    document.querySelectorAll('button').forEach(btn => {
+      btn.onclick = () => resolve(btn.textContent);
+    });
+  });
 }
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
 
   if (humanChoice === 'rock' && computerChoice === 'scissors') {
     return 'You win! Rock beats scissors';
@@ -36,7 +56,7 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-function playGame() {
+async function playGame() {
   let humanSelection = 0;
   let computerSelection = 0;
 
@@ -46,19 +66,21 @@ function playGame() {
 
   let result = 0;
 
+
   function printRoundResult() {
-    console.log('You Chose: ' + humanSelection);
-    console.log('Computer Chose: ' + computerSelection);
-    console.log(result);
+    display.textContent += 'You Chose: ' + humanSelection + '\n' +
+        'Computer Chose: ' + computerSelection + '\n' + result + '\n' +
+        '---------------------------------\n';
   }
 
   for (let i = 0; i < rounds; i++) {
-    humanSelection = getHumanChoice();
+    humanSelection = await getHumanChoice(); // wait for function to finish. only works for async functions
     computerSelection = getComputerChoice();
 
     result = playRound(humanSelection, computerSelection);
 
-    if (result.includes('win')) { // checks if result includes win and ups score
+    if (result.includes(
+            'win')) {  // checks if result includes win and ups score
       printRoundResult();
       humanScore++;
     } else if (result.includes('tie')) {
@@ -70,11 +92,15 @@ function playGame() {
     }
   }
 
+  let message = '';
+
   if (humanScore > computerScore) {  // choose game winner
-    return 'You won the game!';
+    message = ':) You won the game!';
   } else {
-    return 'You lost the game!';
+    message = ':( You lost the game!';
   }
+
+  display.textContent += message;
 }
 
-console.log(playGame());
+playGame();
